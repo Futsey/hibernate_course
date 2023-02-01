59b7a65a52b76c87291700a567a3bf89fc2d6fa5
Fut <shmopls@gmail.com>
2023-02-01 14:41:34 +0500

    gitignore cleaning

--- src/main/java/com/futsey/HibernateRunner.java
+++ src/main/java/com/futsey/HibernateRunner.java
@@ -1,2 +1,18 @@
-package com.futsey;public class HibernateRunner {
+package com.futsey;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+import java.sql.SQLException;
+
+public class HibernateRunner {
+
+    public static void main(String[] args) throws SQLException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+            Session session = sessionFactory.openSession()) {
+            System.out.println("HibernateRunner{ main()}: Project started");
+        };
+    }
 }

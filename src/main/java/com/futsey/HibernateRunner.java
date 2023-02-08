94738e9609503b492cae495750bdf22311401bd2
Fut <shmopls@gmail.com>
2023-02-08 09:52:04 +0500

    Log.console

--- src/main/java/com/futsey/HibernateRunner.java
+++ src/main/java/com/futsey/HibernateRunner.java
@@ -10,6 +10,8 @@ import java.sql.SQLException;
 
 public class HibernateRunner {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) throws SQLException {
 
         User user = User.builder()
@@ -17,39 +19,24 @@ public class HibernateRunner {
                 .firstname("Andrew")
                 .lastname("Petrushin")
                 .build();
+        LOG.info("user entity is in transient state, object {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session_1 = sessionFactory.openSession()) {
-                session_1.beginTransaction();
+            Session session_1 = sessionFactory.openSession();
+            try (session_1) {
+                Transaction transaction = session_1.beginTransaction();
+                LOG.trace("Transaction created, {}", transaction);
 
                 session_1.saveOrUpdate(user);
+                LOG.trace("User {} is in persistent state saved or updated, in session {}", user, session_1);
                 System.out.println("HibernateRunner{main() session_1}: Dirty session: on. User in cash 1: ".concat(String.valueOf(user)));
                 session_1.delete(user);
                 session_1.getTransaction().commit();
             }
-            try (Session session_2 = sessionFactory.openSession()) {
-                session_2.beginTransaction();
-                System.out.println("HibernateRunner{main() session_1}: Dirty session: off. User in DB after session_1.delete(user): "
-                        .concat(String.valueOf(session_2.get(User.class, user.getFirstname()))));
-                user.setFirstname("Ivan");
-                System.out.println("HibernateRunner{main() session_2}: Dirty session: on. User in cash 1 with overridden firstname : "
-                        .concat(String.valueOf(user)));
-                // example .refresh() !!!!!UserInDB важнее User!!!!!
-                    // session_2.refresh(user);
-                    // User userInDB = session_2.get(User.class, user.getFirstname());
-                    // user.setUsername(userInDB.getUsername());
-                    // user.setFirstname(userInDB.getFirstname());
-                    // user.setLastname(userInDB.getLastname());
-                    // etc...
-                // example .merge() !!!!!User важнее userInDB!!!!!
-                    // session_2.merge(user);
-                    // User userInDB = session_2.get(User.class, user.getFirstname());
-                    // userInDB.setUsername(user.getUsername());
-                    // userInDB.setFirstname(user.getFirstname());
-                    // userInDB.setLastname(user.getLastname());
-                    // etc...
-                session_2.getTransaction().commit();
-            }
+            LOG.warn("User {} is in detached state, session {} is closed", user, session_1);
+        } catch (Exception e) {
+            LOG.error("Exception occurred", e);
+            throw e;
         }
     }
 }
